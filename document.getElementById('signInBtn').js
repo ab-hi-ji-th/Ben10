73d9fbd413dd2d96bd4f05efd8494495248dc539
Abhijith <143083208+ab-hi-ji-th@users.noreply.github.com>
2023-11-22 11:50:44 +0530

    Add files via upload

---
+++ document.getElementById('signInBtn').js
@@ -0,0 +1,11 @@
+document.getElementById('signInBtn').addEventListener('click', function() {
+    // Handle the Sign In button click
+    // You can replace the URL with the desired destination
+    window.location.href = 'your-signin-page.html';
+});
+
+document.getElementById('signUpBtn').addEventListener('click', function() {
+    // Handle the Sign Up button click
+    // You can replace the URL with the desired destination
+    window.location.href = 'your-signup-page.html';
+});
---
+++ js_3.js
@@ -0,0 +1,4 @@
+function signIn() {
+    alert('Sign In button clicked!');
+    // You can add additional logic for sign-in functionality
+  }
